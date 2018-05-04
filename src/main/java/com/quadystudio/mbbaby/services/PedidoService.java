7c762f2bc044fe9a6cebed900507439f88be7164
Matheus Silva <matthdeveloper@gmail.com>
2018-05-04 16:09:17 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/quadystudio/mbbaby/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.quadystudio.mbbaby.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quadystudio.mbbaby.domain.Pedido;
+import com.quadystudio.mbbaby.repositories.PedidoRepository;
+import com.quadystudio.mbbaby.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
